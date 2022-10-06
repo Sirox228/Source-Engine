4add9f4dc6ba6d1bc30720518d9c28f76e61c5cb
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-10-06 16:07:27 +0300

    Update GameState.hx

--- source/GameState.hx
+++ source/GameState.hx
@@ -6,7 +6,7 @@ import flixel.util.FlxColor;
 class GameState extends FlxState {
 	public var battle:HornyScript;
 	public var system:HornyScript;
-        public var map:Map<String, Dynamic> = [];
+        public var map:Map<String, Any> = [];
 	
 	override public function create() {
 		super.create();
