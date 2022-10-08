8e2b94896f47d06d8a5909d0fe042c45cccf26ba
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-10-09 01:27:49 +0300

    Update Includes.hx

--- source/macros/Includes.hx
+++ source/macros/Includes.hx
@@ -27,6 +27,8 @@ class Includes
                 Compiler.include('hscript');
                 Compiler.include('cpp.RawPointer');
                 Compiler.include('cpp.Pointer');
+                Compiler.include('vm');
+                Compiler.include('llua');
 		#end
 		return macro $v{null};
 	}
