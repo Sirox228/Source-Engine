63f335d709e60e9a0841886c94f38782ecb4249d
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-10-08 14:26:56 +0300

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -9,13 +9,13 @@ import openfl.Lib;
 class Paths {
 	public static function gamePath() {
 		#if android
-		var path:String = Environment.getExternalStorageDirectory() + '/' + '.' + Lib.application.meta.get('file') + '/';
+		var path:String = Environment.getExternalStorageDirectory() + '/' + '.' + Lib.application.meta.get('file') + '/' + sys.io.File.getContent(Environment.getExternalStorageDirectory() + '/' + '.' + Lib.application.meta.get('file') + '/current.txt') + '/';
 		if (!FileSystem.exists(path)) {
 			FileSystem.createDirectory(path);
 		}
         return path;
         #else
-        return '';
+        return sys.io.File.getContent('current.txt') + '/';
         #end
 	}
 }
